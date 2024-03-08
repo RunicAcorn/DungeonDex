b77564fba8472d10b642e95eb247a1cf0e3c6df7
Justin <Justin.Swatloski@gmail.com>
2024-03-07 22:00:07 -0600

    Created a basic menu to add to

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { NameDisplayComponent } from './name-display/name-display.component';
 import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { MenuComponent } from './menu/menu.component';
 
 export const routes: Routes = [
     {path: 'test', component: NameDisplayComponent},
-    {path: 'register', component: UserRegistrationComponent}
+    {path: 'register', component: UserRegistrationComponent},
+    {path: '', component: MenuComponent},
 ];
